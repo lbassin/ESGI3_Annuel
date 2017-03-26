9237d04dc6e72a3e3d2d391630ea709f7f5a0f58
Laurent Bassin <laurent@bassin.info>
2017-03-26 18:59:45 +0200

    First draft

--- views/frontend.temp.php
+++ views/frontend.temp.php
@@ -4,8 +4,91 @@
         <meta charset="utf-8">
         <title>...</title>
         <meta name="description" content="...">
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,900|Montserrat" rel="stylesheet">
+        <link rel="stylesheet" href="views/style.css">
     </head>
     <body>
-        <?php include $this->view; ?>
+        <nav>
+            <ul>
+                <li class="logo"><img src="http://lorempixel.com/32/32" alt="logo">
+                <li>Try
+                <li>Features
+                <li>Docs
+                <li>Support
+                <li>Blog
+            </ul>
+        </nav>
+        <div class="container">
+            <div class="row">
+                <h1 class="margin-top-large">Version Control Everything.<br>Yes, even your content.</h1>
+                <div class="margin-top-medium">
+                    <p class="text-center">
+                        A true CMS designed to make agencies profitable, developers happy, and clients hug you.
+                    </p>
+                    <p class="margin-top-small text-center">
+                        <button class="btn btn-primary">Buy Now</button>
+                        <button class="btn btn-default">Learn More</button>
+                    </p>
+                </div>
+            </div>
+
+            <hr class="margin-top-medium">
+
+            <div class="row">
+                <h2 class="text-center">Statamic makes teams more productive.</h2>
+                <div class="one-col margin-top-small">
+                    <p class="col medium text-center description">
+                        Every little change — from content to config, users to templates, and everything in between — can be version controlled,
+                        allowing you to build quickly, staying in-sync with live sites, other developers, and client changes.
+                        Design a process that supercharges your team's productivity and adds directly to your bottom-line.
+                    </p>
+                </div>
+                <div class="one-col text-center margin-top-medium">
+                    <img class="col medium" src="https://statamic.com/img/screens/cp-alt.png" alt="">
+                </div>
+            </div>
+
+            <hr class="margin-top-medium"><div class="row">
+                <h2 class="text-center">The features you need are already built in.</h2>
+                <div class="one-col margin-top-small">
+                    <p class="col medium text-center description">
+                        Every little change — from content to config, users to templates, and everything in between — can be version controlled,
+                        allowing you to build quickly, staying in-sync with live sites, other developers, and client changes.
+                        Design a process that supercharges your team's productivity and adds directly to your bottom-line.
+                    </p>
+                </div>
+                <div class="two-col-picture margin-top-medium">
+                    <div class="col large">
+                        <img src="https://statamic.com/img/screens/pages.png" alt="">
+                    </div>
+                    <div class="col small">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aspernatur culpa dolorem est excepturi fuga fugit, id illum iste iure nemo nisi odio quod reiciendis sapiente sed sunt unde veritatis.
+                    </div>
+                </div>
+                <div class="two-col-picture margin-top-medium">
+                    <div class="col small">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aspernatur culpa dolorem est excepturi fuga fugit, id illum iste iure nemo nisi odio quod reiciendis sapiente sed sunt unde veritatis.
+                    </div>
+                    <div class="col large">
+                        <img src="https://statamic.com/img/screens/pages.png" alt="">
+                    </div>
+                </div>
+                <div class="two-col-picture margin-top-medium">
+                    <div class="col large">
+                        <img src="https://statamic.com/img/screens/pages.png" alt="">
+                    </div>
+                    <div class="col small">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aspernatur culpa dolorem est excepturi fuga fugit, id illum iste iure nemo nisi odio quod reiciendis sapiente sed sunt unde veritatis.
+                    </div>
+                </div>
+            </div>
+
+            <hr class="margin-top-medium">
+
+            <div class="row">
+
+            </div>
+        </div>
     </body>
 </html>
