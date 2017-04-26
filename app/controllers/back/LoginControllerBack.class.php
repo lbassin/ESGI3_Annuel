96dd6ad56c775a89125b031be117e3934339e137
fabio ROCCO <fabio.rocco@green-conseil.com>
2017-04-27 01:55:15 +0200

    first version of login action

--- app/controllers/back/LoginControllerBack.class.php
+++ app/controllers/back/LoginControllerBack.class.php
@@ -8,6 +8,27 @@ class LoginControllerBack
         $view = new View('back', 'index', 'login');
     }
 
+    public function loginAction($params)
+    {
+        if (empty($params['email']) || empty($params['password'])) {
+            die('Formulaire de connexion non remplis correctement');
+        }
+        $user = new User();
+        $user->populate(['email' => $params['email'], 'password' => $params['password']]);
+
+        if ($user->getId() < 0) {
+            die('Connexion impossible');
+        } else {
+            // User existe
+            if ($user->getStatus() != 0) {
+                // User actif
+                $view = new View('back', 'index', 'admin');
+                // Lancer la session d'authentification
+                //Csrf::generate();
+            }
+        }
+    }
+
     public function logoutAction()
     {
         session_unset();
