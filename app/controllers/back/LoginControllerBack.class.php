80dcacd2bd8ef0f96f20d95c77f7125cbce04e2a
fabio ROCCO <fabio.rocco@green-conseil.com>
2017-04-27 01:41:48 +0200

    logout action

--- app/controllers/back/LoginControllerBack.class.php
+++ app/controllers/back/LoginControllerBack.class.php
@@ -7,4 +7,11 @@ class LoginControllerBack
     {
         $view = new View('back', 'index', 'login');
     }
+
+    public function logoutAction()
+    {
+        session_unset();
+        session_destroy();
+        $view = new View('back', 'index', 'login');
+    }
 }
