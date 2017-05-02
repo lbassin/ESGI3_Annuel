5e412e09d2e6b95e3dd6a87628429e42742cb575
Laurent Bassin <laurent@bassin.info>
2017-05-02 22:44:51 +0200

    Setup views

--- app/controllers/back/SetupControllerBack.class.php
+++ app/controllers/back/SetupControllerBack.class.php
@@ -3,14 +3,19 @@
 class SetupControllerBack
 {
 
-    public function indexAction($params)
+    public function indexAction()
     {
-        $view = new View('back', 'index', 'setup');
+        $view = new View('back', 'setup/index', 'setup');
     }
 
     public function checkAction()
     {
-        $view = new View('back', 'index', 'setup');
+        $view = new View('back', 'setup/check', 'setup');
+    }
+
+    public function infosAction()
+    {
+        $view = new View('back', 'setup/infos', 'setup');
     }
 
 }
