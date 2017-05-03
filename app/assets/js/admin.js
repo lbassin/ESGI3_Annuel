519fc7e19521cc1788134412c25da940e46e0cc9
maxime <maxime.marquet1@gmail.com>
2017-05-03 19:14:35 +0200

    En cours de debugage

--- app/assets/js/admin.js
+++ app/assets/js/admin.js
@@ -22,8 +22,7 @@ var container = document.querySelector(".container");
 
 
 if (navToggle != null) {
-    navToggle.addEventListener("click", function()
-    {
+    navToggle.addEventListener("click", function () {
         navToggle.classList.toggle('open');
         container.classList.toggle("slide-out-container");
         slideNavLarge.classList.toggle("slide-out");
@@ -31,13 +30,62 @@ if (navToggle != null) {
         slideNavSmall.classList.toggle("is-not-visible");
         //slideNavMini.style.display = 'none';
     });
-}
 
-navSettingsToggle.addEventListener("click", function () {
-    settingsNav.classList.toggle("is-visible");
-});
+    navSettingsToggle.addEventListener("click", function () {
+        settingsNav.classList.toggle("is-visible");
+    });
+}
 
 var flashMessages = document.querySelector('.flash-messages');
 setTimeout(function () {
     flashMessages.className += ' is-not-visible';
 }, 2500);
+
+
+var passwordForgetButton = document.querySelector("#forget-password-button");
+var backLoginButton = document.querySelector("#back-login-button");
+var loginForm = document.querySelector("#container-login-form");
+var passwordForgetForm = document.querySelector("#container-password-forget");
+
+passwordForgetButton.addEventListener("click", function(){
+    fadeOut(loginForm);
+    setTimeout(function(){
+        fadeIn(passwordForgetForm);
+    }, 250);
+});
+
+backLoginButton.addEventListener("click", function(){
+    fadeOut(passwordForgetForm);
+    setTimeout(function(){
+        fadeIn(loginForm);
+    }, 250);
+});
+
+function fadeOut(el){
+    el.style.opacity = 1;
+
+    (function fade() {
+        if ((el.style.opacity -= .1) < 0) {
+            el.style.display = 'none';
+            el.classList.add('is-hidden');
+        } else {
+            requestAnimationFrame(fade);
+        }
+    })();
+}
+
+function fadeIn(el, display){
+    if (el.classList.contains('is-hidden')){
+        el.classList.remove('is-hidden');
+    }
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+
+    (function fade() {
+        var val = parseFloat(el.style.opacity);
+        if (!((val += .1) > 1)) {
+            el.style.opacity = val;
+            requestAnimationFrame(fade);
+        }
+    })();
+}
--- app/views/login.temp.php
+++ app/views/login.temp.php
@@ -7,9 +7,53 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 </head>
 <body>
+
+<?php
+
+//$mail = new Mail();
+//$mail->send('maxime.marquet1@gmail.com', 'sujet', 'message');
+
+
+
+require 'app/assets/lib/PHPMailer/PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 2;
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.qwarkz.fr;';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'noreply@qwarkz.fr';                 // SMTP username
+$mail->Password = 'azertyuiop';                           // SMTP password
+$mail->SMTPSecure = false;                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 585;                                    // TCP port to connect to
+
+$mail->setFrom('noreply@qwarkz.fr', 'Qwarkz');
+$mail->addAddress('maxime.marquet1@gmail.com');     // Add a recipient
+/*$mail->addReplyTo('info@example.com', 'Information');
+$mail->addCC('cc@example.com');
+$mail->addBCC('bcc@example.com');*/
+
+/*$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name*/
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.<br>';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+?>
 <div class="container" style="height: 100vh;display: flex;align-items: center;justify-content: center;">
-    <div class="container-login">
+    <span class="container-login">
         <?php include 'app/assets/logo.html'; ?>
+        <span id="container-login-form" class="container-login-form">
         <div class="field-container">
             <input type="text" placeholder="Identifiant">
         </div>
@@ -19,6 +63,21 @@
         </div>
 
         <input type="submit" class="login-submit" value="Se connecter">
+        <a id="forget-password-button" class="link-password-forget button-login-page">
+            <span class="hover-link-center">Mot de passe oublié</span>
+        </a>
+        </span>
+
+        <span id="container-password-forget" class="container-login-form">
+            <div class="field-container">
+                <input type="password" placeholder="Adresse email">
+            </div>
+
+            <input type="submit" class="login-submit" value="Envoyer">
+            <a id="back-login-button" class="back-login-button button-login-page">
+                <span class="hover-link-center">Retour</span>
+            </a>
+        </span>
     </div>
 </div>
 
