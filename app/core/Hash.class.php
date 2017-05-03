0daa662bd605225110b20c2b858f15b896eb51cd
fabio ROCCO <fabio.rocco@green-conseil.com>
2017-05-03 10:59:46 +0200

    Add hashing class

---
+++ app/core/Hash.class.php
@@ -0,0 +1,24 @@
+<?php
+class Hash
+{
+    /**
+     * Return a hash version of the password with the bluefish algorythm
+     */
+    static function generate($sPassword)
+    {
+        $aOptions = [
+            'cost' => 11
+        ];
+        return password_hash($sPassword, PASSWORD_BCRYPT, $aOptions);
+    }
+    // ---
+    /*
+        Return true if the password is confirm, false if not
+     */
+    static function check($sPassword, $sHash)
+    {
+        echo 1;
+        return (password_verify($sPassword, $sHash) === true) ? true : false ;
+    }
+    // ---
+}
