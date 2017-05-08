01b981eb5336827097027e7a8e64b67fc7e409d1
Laurent Bassin <laurent@bassin.info>
2017-05-08 13:13:58 +0200

    Setup scss

--- app/views/setup.temp.php
+++ app/views/setup.temp.php
@@ -9,27 +9,12 @@
 
     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
     <link rel="stylesheet" href="app/assets/css/setup.css">
-    <link rel="stylesheet" href="app/assets/css/admin.css">
-    <link rel="stylesheet" href="app/assets/css/logo.css">
 </head>
 <body>
 <div class="setup">
-    <header>
-        <div class="logo-container">
-            <div class="logo">
-                <svg width="200px" height="200px">
-                    <path id="logo-circle-1" stroke="#000" stroke-width="0" fill="#a2a2a2" d="M140.773,59.227C137.316,55.771,130.055,50,100,50
-                    s-37.317,5.771-40.774,9.227C55.77,62.684,49.999,69.104,50,100c-0.001,30.896,5.77,37.316,9.227,40.773
-                    C62.683,144.229,69.103,150,100,150c30.895,0,37.317-5.771,40.772-9.227C144.229,137.316,150,130.896,150,100
-                    S144.229,62.683,140.773,59.227z"/>
+    <?php require 'app/assets/logo.html'; ?>
 
-                    <path id="logo-circle-2" stroke="#000" stroke-width="0" fill="#a2a2a2" d="M140.773,59.227C137.316,55.771,130.055,50,100,50
-                    s-37.317,5.771-40.774,9.227C55.77,62.684,49.999,69.104,50,100c-0.001,30.896,5.77,37.316,9.227,40.773
-                    C62.683,144.229,69.103,150,100,150c30.895,0,37.317-5.771,40.772-9.227C144.229,137.316,150,130.896,150,100
-                    S144.229,62.683,140.773,59.227z"/>
-                </svg>
-            </div>
-        </div>
+    <div class="header">
         <div class="breadcrumbs"></div>
         <div class="status-progress">
             <ul class="clearfix">
@@ -41,7 +26,7 @@
                 <?php endfor; ?>
             </ul>
         </div>
-    </header>
+    </div>
     <div class="content">
         <?php include $this->view; ?>
     </div>
