28eba208661fbc18f57abe42577ae4bbe855362c
Laurent Bassin <laurent@bassin.info>
2017-05-11 22:29:31 +0200

    Split login into template/view

--- app/views/login/index.view.php
+++ app/views/login/index.view.php
@@ -1,1 +1,21 @@
-<h1>Login</h1>
+<div class="container" style="height: 100vh;display: flex;align-items: center;justify-content: center;">
+    <form class="container-login">
+        <?php include 'app/assets/logo.html'; ?>
+        <div class="field-container">
+            <input type="text" name="email" placeholder="Identifiant">
+        </div>
+        <br>
+        <div class="field-container">
+            <input type="password" name="password" placeholder="Mot de passe">
+        </div>
+
+        <input type="submit" class="login-submit" value="Se connecter">
+    </form>
+</div>
+
+<script>
+    var loginUrlPost = '<?php echo Helpers::getAdminRoute('login/login'); ?>';
+    var csrfToken = '<?php echo $csrfToken; ?>';
+</script>
+
+<script type="text/javascript" src="<?php echo Helpers::getAsset('js/login.js'); ?>"></script>
