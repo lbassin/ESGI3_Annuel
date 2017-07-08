27c2f6b12d07b3c9f694d33f3e70f3c787c9a5d4
Laurent Bassin <laurent@bassin.info>
2017-07-08 16:55:53 +0200

    Remove unused co

--- app/views/comment/index.view.php
+++ app/views/comment/index.view.php
@@ -1,6 +1,3 @@
 <?php if (isset($comment)): ?>
     <?php $this->includeModal('list', $comment->getListConfig(), $configList); ?>
 <?php endif; ?>
-
-<br>
-    <a href="#">Supprimer les Commentaires séléctionnés</a>
