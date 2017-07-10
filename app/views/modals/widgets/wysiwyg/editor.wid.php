b9bac116c184577ad89b71839897257d23e789ee
Laurent Bassin <laurent@bassin.info>
2017-07-10 18:10:17 +0200

    Quill JS - WYSIWYG

---
+++ app/views/modals/widgets/wysiwyg/editor.wid.php
@@ -0,0 +1,5 @@
+<div id="editor"></div>
+<div>Editeur</div>
+<input type="hidden" name="editor" id="editor-input" value="">
+
+<script src="<?php echo Helpers::getAsset('js/modals/widgets/wysiwyg.js'); ?>"></script>
