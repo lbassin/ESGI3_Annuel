2032279663654fa4a4ae21e99cf13a7ec1e7745e
Jérémy Bachayani <jeremy.bachayani@gmail.com>
2017-07-14 23:25:00 +0200

    add html structure

---
+++ themes/templates/default/frontend.temp2.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="<?php echo Helpers::getThemeAsset('css/theme.css'); ?>">
+</head>
+<body>
+<div class="container">
+    <div class="top-container">
+        <div class="top-content-container">
+            <div class="top-title">
+                <h1>Titre</h1>
+            </div>
+            <div class="top-content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiore
+                    blanditiis corporis debitis delectus dolore in, ipsam ipsum iure libero minima nesciunt nostrum, odit,
+                    omnis quas repudiandae sed tempora voluptatem?
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="container-cases">
+        <div class="cases">
+            <div class="case">
+                <div class="case-avatar">
+                    <img src="http://lorempicsum.com/nemo/255/200/5" alt="">
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aperiam dolorem dolorum eius est in maiores molestiae neque nostrum officia perferendis porro praesentium provident quas qui recusandae, reiciendis totam voluptates.</p>
+                <a href="#">Lien ...</a>
+            </div>
+            <div class="case">
+                <div class="case-avatar">
+                    <img src="http://lorempicsum.com/nemo/255/200/5" alt="">
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aperiam dolorem dolorum eius est in maiores molestiae neque nostrum officia perferendis porro praesentium provident quas qui recusandae, reiciendis totam voluptates.</p>
+                <a href="#">Lien ...</a>
+            </div>
+            <div class="case">
+                <div class="case-avatar">
+                    <img src="http://lorempicsum.com/nemo/255/200/5" alt="">
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aperiam dolorem dolorum eius est in maiores molestiae neque nostrum officia perferendis porro praesentium provident quas qui recusandae, reiciendis totam voluptates.</p>
+                <a href="#">Lien ...</a>
+            </div>
+        </div>
+    </div>
+    <div class="middle-container">
+        <div class="middle-title">
+            <h2>Titre</h2>
+        </div>
+        <div class="middle-content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiore
+                blanditiis corporis debitis delectus dolore in, ipsam ipsum iure libero minima nesciunt nostrum, odit,
+                omnis quas repudiandae sed tempora voluptatem?
+            </p>
+        </div>
+        <div class="middle-image">
+            <img src="https://statamic.com/img/screens/cp-alt.png" alt="">
+        </div>
+    </div>
+    <div class="bottom-container">
+        <div class="bottom-image">
+            <img src="https://statamic.com/img/screens/pages.png" alt="">
+        </div>
+        <div class="bottom-content">
+            <h2>Titre</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam asperiore
+                blanditiis corporis debitis delectus dolore in, ipsam ipsum iure libero minima nesciunt nostrum, odit,
+                omnis quas repudiandae sed tempora voluptatem?
+            </p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
