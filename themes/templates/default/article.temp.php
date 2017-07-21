7f69d25895a6460d8c9d93ee6d78007b6f565d32
Jérémy Bachayani <jeremy.bachayani@gmail.com>
2017-07-21 15:37:53 +0200

    add article

---
+++ themes/templates/default/article.temp.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="<?php echo Helpers::getThemeAsset('css/article.css'); ?>">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <title>Article</title>
+</head>
+<body>
+<div class="container">
+    <section class="article-container">
+        <header>
+            <div class="article-title">
+
+                <h1>Titre de l'article</h1>
+            </div>
+        </header>
+        <article class="article-content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aliquid asperiores aspernatur assumenda blanditiis cum, debitis
+                dolores ducimus eius eos error esse illo, incidunt libero neque nobis optio repellat! Culpa, iure?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aliquid asperiores aspernatur assumenda blanditiis cum, debitis
+                dolores ducimus eius eos error esse illo, incidunt libero neque nobis optio repellat! Culpa, iure?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aliquid asperiores aspernatur assumenda blanditiis cum, debitis
+                dolores ducimus eius eos error esse illo, incidunt libero neque nobis optio repellat! Culpa, iure?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aliquid asperiores aspernatur assumenda blanditiis cum, debitis
+                dolores ducimus eius eos error esse illo, incidunt libero neque nobis optio repellat! Culpa, iure?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aliquid asperiores aspernatur assumenda blanditiis cum, debitis
+                dolores ducimus eius eos error esse illo, incidunt libero neque nobis optio repellat! Culpa, iure?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aliquid asperiores aspernatur assumenda blanditiis cum, debitis
+                dolores ducimus eius eos error esse illo, incidunt libero neque nobis optio repellat! Culpa, iure?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aliquid asperiores aspernatur assumenda blanditiis cum, debitis
+                dolores ducimus eius eos error esse illo, incidunt libero neque nobis optio repellat! Culpa, iure?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aliquid asperiores aspernatur assumenda blanditiis cum, debitis
+                dolores ducimus eius eos error esse illo, incidunt libero neque nobis optio repellat! Culpa, iure?
+            </p>
+            <div class="article-details">
+                <span>Ecrit par François Hollande le 01 Janvier 2017</span>
+            </div>
+        </article>
+    </section>
+</div>
+</body>
+</html>
