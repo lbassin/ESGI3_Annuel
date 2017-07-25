b244cfd2d27351cccff7b6f04051714c4aa974dc
maxime <maxime.marquet1@gmail.com>
2017-07-25 23:40:07 +0200

    fix chart resize et positionnement de nouvelles charts en attente de data

--- app/controllers/back/IndexControllerBack.class.php
+++ app/controllers/back/IndexControllerBack.class.php
@@ -5,5 +5,7 @@ class IndexControllerBack
     public function indexAction()
     {
         $view = new View('back', 'dashboard/index', 'admin');
+
+        $view->assign('pseudo', 'test');
     }
 }
