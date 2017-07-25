14426f6acdd09ea898355c04adecbda0cbc5587d
maxime <maxime.marquet1@gmail.com>
2017-07-25 22:48:25 +0200

    initialisation highcharts et premier test de données

--- app/views/dashboard/index.view.php
+++ app/views/dashboard/index.view.php
@@ -1,1 +1,6 @@
 <h1>Dashboard</h1>
+
+<div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+
+<script type="text/javascript" src="<?php echo Helpers::getAsset('js/highcharts.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo Helpers::getAsset('js/modals/widgets/dashboard.js'); ?>"></script>
