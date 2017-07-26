6fd0a07f7a704f5bfcafe2c440c0e0270b9a9685
Jérémy Bachayani <jeremy.bachayani@gmail.com>
2017-07-26 11:43:32 +0200

    fix article.sql

--- app/assets/setup/schema/article.sql
+++ app/assets/setup/schema/article.sql
@@ -10,6 +10,6 @@ CREATE TABLE `article` (
   `id_survey`   INT(11),
   `created_at`  TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at`  TIMESTAMP,
-  CONSTRAINT FK_ARTICLE_USER FOREIGN KEY (id_user) REFERENCES user (id),
+  CONSTRAINT FK_ARTICLE_USER FOREIGN KEY (id_user) REFERENCES user (id)
   --CONSTRAINT FK_ARTICLE_SURVEY FOREIGN KEY (id_survey) REFERENCES survey (id)
 );
