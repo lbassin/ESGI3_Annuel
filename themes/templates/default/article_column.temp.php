34179a59f4744326af242d85aaa2212d8893d7c2
Jérémy Bachayani <jeremy.bachayani@gmail.com>
2017-07-26 12:28:18 +0200

    add article column temp

---
+++ themes/templates/default/article_column.temp.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="<?php echo Helpers::getThemeAsset('css/article_column.css'); ?>">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <title>Home</title>
+</head>
+<body>
+<div class="container">
+    <div class="container-cases">
+        <div class="cases">
+            <div class="case">
+                <div class="case-avatar">
+                    <img src="http://lorempicsum.com/nemo/255/200/5" alt="">
+                </div>
+                <div class="case-content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aperiam dolorem dolorum eius est in maiores molestiae neque nostrum officia perferendis porro praesentium provident quas qui recusandae, reiciendis totam voluptates.</p>
+                    <a href="#">Lien ...</a>
+                </div>
+            </div>
+            <div class="case">
+                <div class="case-avatar">
+                    <img src="http://lorempicsum.com/nemo/255/200/5" alt="">
+                </div>
+                <div class="case-content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aperiam dolorem dolorum eius est in maiores molestiae neque nostrum officia perferendis porro praesentium provident quas qui recusandae, reiciendis totam voluptates.</p>
+                    <a href="#">Lien ...</a>
+                </div>
+            </div>
+            <div class="case">
+                <div class="case-avatar">
+                    <img src="http://lorempicsum.com/nemo/255/200/5" alt="">
+                </div>
+                <div class="case-content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aperiam dolorem dolorum eius est in maiores molestiae neque nostrum officia perferendis porro praesentium provident quas qui recusandae, reiciendis totam voluptates.</p>
+                    <a href="#">Lien ...</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
