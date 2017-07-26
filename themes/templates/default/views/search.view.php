4fd94e9da54c575fdca3a5c63642512e0ad16960
fabio ROCCO <fabio.rocco@green-conseil.com>
2017-07-26 11:32:04 +0200

    Page research

--- themes/templates/default/views/search.view.php
+++ themes/templates/default/views/search.view.php
@@ -1,23 +1,20 @@
 <section class="view-container">
     <form action="" method="get" class="form-container">
-        <h2>Recherche</h2>
+        <h2>Recherche d'un article</h2>
         <input type="text" placeholder="Search" name="search" value="<?php echo ((isset($search)) ? $search : ''); ?>">
-        <button type="submit">Rechercher</button>
+        <button type="submit"><i class="fa fa-search loupe" aria-hidden="true"></i>Rechercher</button>
     </form>
     <?php if (isset($pages) || isset($articles)): ?>
-        <?php if (!empty($pages)): ?>
-            <?php foreach ($pages as $page): ?>
-                <a href="page/<?php echo $page->url(); ?>" class="view-case">
-                    <div class="view-case-image"></div>
-                    <div class="view-case-content">
-                        <h3><?php echo $page->title(); ?></h3>
-                        <p><?php echo $page->description(); ?></p>
-                    </div>
-                </a>
-            <?php endforeach; ?>
-        <?php else: ?>
-            Aucune page trouvée
-        <?php endif; ?>
+        <div class="page-container">
+            <h2>Page correspondant :</h2>
+            <?php if (!empty($pages)): ?>
+                <?php foreach ($pages as $page): ?>
+                    <a href="<?php echo $page->url(); ?>"><?php echo $page->title(); ?></a><br>
+                <?php endforeach; ?>
+            <?php else: ?>
+                Aucune page trouvée
+            <?php endif; ?>
+        </div>
         <?php if (!empty($articles)): ?>
             <?php foreach ($articles as $article): ?>
                 <a href="article/<?php echo $article->url(); ?>" class="view-case">
