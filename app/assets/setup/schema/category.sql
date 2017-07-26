6558339efb4758183237485dfb56161e062d012b
Laurent <laurent@bassin.info>
2017-07-26 13:00:11 +0200

    category image

--- app/assets/setup/schema/category.sql
+++ app/assets/setup/schema/category.sql
@@ -3,6 +3,7 @@ CREATE TABLE `category` (
   `title`       VARCHAR(255)        NOT NULL,
   `description` VARCHAR(255)        NOT NULL,
   `url`         VARCHAR(255)        NOT NULL,
+  `path_image1` VARCHAR(255)        NOT NULL,
   `created_at`  TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at`  TIMESTAMP
 );
